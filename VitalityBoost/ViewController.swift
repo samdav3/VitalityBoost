94b73581a6893fb12ea22da414c22e00f117403c
Sam Davenport <sdavenport@students.mchenry.edu>
2025-03-02 14:43:25 -0600

    Initial Commit

---
+++ VitalityBoost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VitalityBoost
+//
+//  Created by Sam on 3/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
