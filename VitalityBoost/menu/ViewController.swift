3d731711451877315d944020cd8557f4922a38a9
Sam Davenport <sdavenport@students.mchenry.edu>
2025-03-04 12:08:17 -0600

    Navigation Updates
    Correcting issues with Nav

--- VitalityBoost/menu/ViewController.swift
+++ VitalityBoost/menu/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        performSegue(withIdentifier: "home", sender: UIButton.self)
+        performSegue(withIdentifier: "login", sender: UIButton.self)
+        performSegue(withIdentifier: "fitness", sender: UIButton.self)
+        performSegue(withIdentifier: "workouts", sender: UIButton.self)
+        performSegue(withIdentifier: "recipes", sender: UIButton.self)
+        performSegue(withIdentifier: "calendar", sender: UIButton.self)
+        performSegue(withIdentifier: "journal", sender: UIButton.self)
+    }
 
 }
 
