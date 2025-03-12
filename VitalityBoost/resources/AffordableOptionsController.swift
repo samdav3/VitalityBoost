edbd3f7fce1fc5fdf9c370a2f8438c401b014b88
Sam Davenport <sdavenport@students.mchenry.edu>
2025-03-12 09:47:37 -0500

    Table Cells Updates
    Adding Table Cells for affordable options and fitness resources

--- VitalityBoost/resources/AffordableOptionsController.swift
+++ VitalityBoost/resources/AffordableOptionsController.swift
@@ -12,7 +12,8 @@ class AffordableOptionsController: UIViewController, UITableViewDelegate, UITabl
     @IBOutlet var mainView: UIView!
     @IBOutlet weak var subView: UIView!
     @IBOutlet weak var table: UITableView!
-    let affTableArray = ["", "", "", ""]
+    let affTitleArray = ["Free At Home Workouts", "Better Me", "Apple Fitness", "DAREBEE", "Planet Fitness", "The Edge Fitness Clubs", "MadFit YouTube Channel", "Tone & Tighten YouTube Channel", "growingannanas YouTube Channel", ""]
+    let affDetailArray = ["https://www.muscleandstrength.com/workouts/home", "https://betterme.world/", "https://www.apple.com/apple-fitness-plus/", "https://darebee.com/workouts.html", "https://www.planetfitness.com/", "https://www.theedgefitnessclubs.com/", "https://www.youtube.com/@MadFit", "https://www.youtube.com/@toneandtighten", "https://www.youtube.com/@growingannanas", ""]
     let cellID = "cellID"
     
     override func viewDidLoad() {
@@ -27,18 +28,19 @@ class AffordableOptionsController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return affTableArray.count
+        return affTitleArray.count
         }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
         if (cell == nil){
             cell = UITableViewCell(
-                style: UITableViewCell.CellStyle.default,
+                style: UITableViewCell.CellStyle.subtitle,
                 reuseIdentifier: cellID
             )
         }
-        cell?.textLabel?.text = affTableArray[indexPath.row]
+        cell?.textLabel?.text = affTitleArray[indexPath.row]
+        cell?.detailTextLabel?.text = affDetailArray[indexPath.row]
         return cell!
         }
         
--- VitalityBoost/resources/FitnessController.swift
+++ VitalityBoost/resources/FitnessController.swift
@@ -13,7 +13,9 @@ class FitnessController: UIViewController, UITableViewDelegate, UITableViewDataS
     @IBOutlet var mainView: UIView!
     @IBOutlet weak var subView: UIView!
     @IBOutlet weak var table: UITableView!
-    let fitTableArray = ["", "", "", ""]
+    let fitTitleArray = ["Exercise Is Medicine", "Harvard: Exercise & Fitness Benefits", "NIH: Health Benefits of Exercise", "CDC: Benefits of Physical Activity"]
+    let fitDetailArray = ["https://www.amazon.com/Exercise-Medicine-Physical-Activity-Boosts/dp/0190685468/ref=asc_df_0190685468?mcid=b31fd8ee41c53e3b9a2d9ec70834fbb9&tag=hyprod-20&linkCode=df0&hvadid=693552282123&hvpos=&hvnetw=g&hvrand=10105925173890605268&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9021434&hvtargid=pla-872611948063&psc=1", "https://www.health.harvard.edu/topics/exercise-and-fitness", "https://pmc.ncbi.nlm.nih.gov/articles/PMC6027933/", "https://www.cdc.gov/physical-activity-basics/benefits/index.html"]
+    //let imageArray = ["exercise-med.png", "harvard-health.png", "nat-lib-med.png"]
     let cellID = "cellID"
     
     override func viewDidLoad() {
@@ -28,18 +30,21 @@ class FitnessController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fitTableArray.count
+        return fitTitleArray.count
         }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
         if (cell == nil){
             cell = UITableViewCell(
-                style: UITableViewCell.CellStyle.default,
+                style: UITableViewCell.CellStyle.subtitle,
                 reuseIdentifier: cellID
             )
         }
-        cell?.textLabel?.text = fitTableArray[indexPath.row]
+        
+        cell?.textLabel?.text = fitTitleArray[indexPath.row]
+        cell?.detailTextLabel?.text = fitDetailArray[indexPath.row]
+        //cell?.imageView?.image = UIImage(named: imageArray[indexPath.row])
         return cell!
         }
     
